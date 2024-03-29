2c3931b3e49089071739b7460dba31a173351269
Weixin Christina Yan <christinayan2001@gmail.com>
2024-03-29 11:29:33 +0100

    what is this

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 import {resolve} from 'path';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  target: 'static',
+  router: {
+    base: '/xtinayan.github.io/'
+  },
   alias:{
     '@':resolve(__dirname, "/"),
   },
