f3bc71f76d9db617a67791c6008ae33fdf75595a
Weixin Christina Yan <christinayan2001@gmail.com>
2025-07-17 18:13:33 +0800

    added animation to home page

--- pages/about.vue
+++
@@ -1,7 +0,0 @@
-<template> 
-    <div> 
-        <h2>Hi!</h2>
-        <br>
-        <p>(Placeholder since i have no idea what to write)</p>
-    </div>
-</template>
--- pages/journals/index.vue
+++
@@ -1,8 +0,0 @@
-<template> 
-    <div> 
-        <h2>coming soon...</h2>
-        <br>
-        <p>stay tuned :)</p>
-    
-    </div>
-</template>
