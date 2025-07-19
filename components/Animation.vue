ec39370595d16b9d617704e652a73676fd690418
Weixin Christina Yan <christinayan2001@gmail.com>
2025-07-19 09:49:14 +0800

    updated animation

--- components/Animation.vue
+++ components/Animation.vue
@@ -1,19 +1,39 @@
 <template>
-  <div class="flex justify-center items-center w-full h-full min-h-[300px]">
-    <transition name="fade" mode="out-in">
+  <div class="flex flex-col justify-center items-center w-full min-h-[400px] text-center">
+    <transition name="fade" mode="out-in" @after-enter="onAfterEnter">
+      <!-- animation  -->
       <template v-if="!showText">
         <img
           :key="'image'"
           :src="currentImage"
           alt="Stop motion"
           class="w-full max-w-md h-auto object-contain mx-auto rounded-lg cursor-pointer"
-          @click="handleClick"
+          @click="toggleToText"
         />
       </template>
+
+      <!-- text -->
       <template v-else>
-        <div :key="'text'" class="text-center px-4">
-          <h2 class="text-xl font-semibold mb-2">welcome to chrissy's corner :)</h2>
-          <p class="text-base">(still don't know what else to write)</p>
+        <div :key="'text'" class="px-4 cursor-pointer" @click="toggleToImages">
+          <h2 class="text-xl font-semibold mb-5">click to see the animation again:)</h2>
+          <p class="text-xl font-semibold mb-2">or explore the page ↓</p>
+          <div class="flex justify-center space-x-4 text-sm">
+            <NuxtLink to="/" class="px-3 py-1 bg-gray-100 rounded hover:bg-gray-200">
+              Home
+            </NuxtLink>
+            <NuxtLink
+              to="/journal"
+              class="px-3 py-1 bg-gray-100 rounded hover:bg-gray-200"
+            >
+              Journal
+            </NuxtLink>
+            <NuxtLink
+              to="/gallery"
+              class="px-3 py-1 bg-gray-100 rounded hover:bg-gray-200"
+            >
+              Gallery
+            </NuxtLink>
+          </div>
         </div>
       </template>
     </transition>
@@ -49,6 +69,50 @@ let finished = false;
 const stopAtIndex =
   props.finalImageIndex !== null ? props.finalImageIndex : props.images.length - 1;
 
+const startAnimation = () => {
+  stopAnimation(); // Clear any existing interval
+  index.value = 0;
+  currentImage.value = props.images[0];
+
+  intervalId = setInterval(() => {
+    if (index.value >= stopAtIndex) {
+      stopAnimation();
+      emit("finished");
+    } else {
+      index.value++;
+      currentImage.value = props.images[index.value];
+    }
+  }, props.interval);
+};
+
+const stopAnimation = () => {
+  if (intervalId) {
+    clearInterval(intervalId);
+    intervalId = null;
+  }
+};
+
+const toggleToText = () => {
+  stopAnimation();
+  showText.value = true;
+};
+
+// make sure the transition time is taken into account before starting animation
+const transitioningToImages = ref(false);
+
+const onAfterEnter = () => {
+  if (transitioningToImages.value) {
+    transitioningToImages.value = false;
+    startAnimation(); // Start animation AFTER the transition
+  }
+};
+
+const toggleToImages = () => {
+  transitioningToImages.value = true;
+  showText.value = false;
+  // startAnimation();
+};
+
 const switchImage = () => {
   if (index.value >= stopAtIndex) {
     clearInterval(intervalId);
@@ -60,12 +124,11 @@ const switchImage = () => {
   index.value++;
   currentImage.value = props.images[index.value];
 };
-
-const handleClick = () => {
-  if (finished) {
-    showText.value = true;
-  }
-};
+// const handleClick = () => {
+//   if (finished && canClick.value) {
+//     showText.value = true;
+//   }
+// };
 
 onMounted(() => {
   if (props.images.length > 1) {
