2a49cd3cbc592f4e2a0e454977ab6b5147d0fefa
Weixin Christina Yan <christinayan2001@gmail.com>
2025-07-19 17:07:08 +0800

    added new pic

--- pages/journal/index.vue
+++ pages/journal/index.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="flex flex-col items-center text-center pt-[10vh]">
     <a href="https://xtinayan.substack.com/" target="_blank" rel="noopener noreferrer">
-      <h2 class="text-2xl font-bold underline">My FTTL updates</h2>
+      <h2 class="text-2xl font-bold">my FTTL updates</h2>
     </a>
 
     <p>(warning: i'm not the best at writing updates)</p>
 
-    <img
-      src="/images/bowerhouse.webp"
-      alt="Journal Image"
-      class="my-4 max-h-[50vh] rounded-md"
-    />
+    <a href="https://xtinayan.substack.com/" target="_blank" rel="noopener noreferrer">
+      <img
+        src="/images/bowerhouse.webp"
+        alt="Journal Image"
+        class="my-4 max-h-[50vh] rounded-md"
+      />
+    </a>
   </div>
 </template>
